98437fddabd921f8f7c87386f03b92b9737b7091
Harsha M <67669350+harshamullapudi@users.noreply.github.com>
2024-10-10 21:03:57 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!usr/bin/bash
 apt update
 #python3.8 installation
-apt install -y python3.8 python3.8-dev
+apt install -y python3.8
 ln -sf /usr/bin/python3.8 /usr/bin/python3
 apt install -y python3-pip
 #python3.8 -m pip install --upgrade pip
